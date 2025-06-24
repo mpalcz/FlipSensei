a621709ac038b93b10c624b5273366ecdcfc07c0
Nzouh <nzouh055@uottawa.ca>
2025-06-23 23:47:05 -0400

    Adding basic scraping functionality, currently limited to console on web

--- scraper.js
+++ scraper.js
@@ -1,3 +1,13 @@
-console.log("HELLO WORLD");
-console.log("Hey Mounir buy my product!")
-console.log("Hey Megan wsp")
+console.log("🔥 FlipSensei script injected 🔥");
+
+
+// Since Facebook has multiple nested divs and dynamic class names, must find another way to scrape:
+setTimeout(() => {
+    const items = document.querySelectorAll("a[href^='/marketplace/item/']"); //Scrapes all the items with the a tag and corresponding href
+    console.log("Found ${items.length} items!") //prints amount of items scraped
+    items.forEach(el => {
+        console.log("--------");
+        console.log(el.innerText);
+    })}, 3000);
+
+console.log("FlipSensei Script Loaded!");
